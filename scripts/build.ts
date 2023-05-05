e9203bd059bd1c34024858e7c56c5bd3220cec9f
benten <ben@benten.ca>
2023-05-05 14:09:46 -0600

    Hello world

---
+++ scripts/build.ts
@@ -0,0 +1,24 @@
+import { build, stop } from "esbuild"
+import { lamina_esbuild_solid } from "./plugin.ts";
+
+export async function build_project() {
+    await build({
+        entryPoints: [ 
+            "src/index.tsx",
+        ],
+        bundle: true,
+        allowOverwrite: true,
+        outfile: "public/index.js",
+        platform: "browser",
+        format: "esm",
+        target: "esnext",
+        plugins: [
+            lamina_esbuild_solid({
+                config: "./import_map.json"
+            })
+        ],
+    })
+    stop()
+}
+
+build_project()
